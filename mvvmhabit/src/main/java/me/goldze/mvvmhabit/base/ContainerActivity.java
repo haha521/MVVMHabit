9f21f2021c71567f6e95c742f27c0e319dbbeebd
zh <zhuhao@firstarr.com>
2021-05-13 14:33:07 +0800

    编辑框软键盘

---
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/base/ContainerActivity.java
@@ -0,0 +1,141 @@
+package me.goldze.mvvmhabit.base;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+import java.lang.ref.WeakReference;
+
+import me.goldze.mvvmhabit.R;
+
+import static android.view.View.generateViewId;
+
+
+/**
+ * 盛装Fragment的一个容器(代理)Activity
+ * 普通界面只需要编写Fragment,使用此Activity盛装,这样就不需要每个界面都在AndroidManifest中注册一遍
+ */
+public class ContainerActivity extends RxAppCompatActivity {
+    private static final String FRAGMENT_TAG = "content_fragment_tag";
+    public static final String FRAGMENT = "fragment";
+    public static final String BUNDLE = "bundle";
+    protected WeakReference<Fragment> mFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = null;
+        if (savedInstanceState != null) {
+            fragment = fm.getFragment(savedInstanceState, FRAGMENT_TAG);
+        }
+        if (fragment == null) {
+            fragment = initFromIntent(getIntent());
+        }
+        FragmentTransaction trans = getSupportFragmentManager()
+                .beginTransaction();
+        trans.replace(R.id.content, fragment);
+        trans.commitAllowingStateLoss();
+        mFragment = new WeakReference<>(fragment);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, FRAGMENT_TAG, mFragment.get());
+    }
+
+    protected Fragment initFromIntent(Intent data) {
+        if (data == null) {
+            throw new RuntimeException(
+                    "you must provide a page info to display");
+        }
+        try {
+            String fragmentName = data.getStringExtra(FRAGMENT);
+            if (fragmentName == null || "".equals(fragmentName)) {
+                throw new IllegalArgumentException("can not find page fragmentName");
+            }
+            Class<?> fragmentClass = Class.forName(fragmentName);
+            Fragment fragment = (Fragment) fragmentClass.newInstance();
+            Bundle args = data.getBundleExtra(BUNDLE);
+            if (args != null) {
+                fragment.setArguments(args);
+            }
+            return fragment;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException("fragment initialization failed!");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.content);
+        if (fragment instanceof BaseFragment) {
+            if (!((BaseFragment) fragment).isBackPressed()) {
+                super.onBackPressed();
+            }
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideKeyboard(v, ev)) {
+                hideKeyboard(v.getWindowToken());
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private boolean isShouldHideKeyboard(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0],
+                    top = l[1],
+                    bottom = top + v.getHeight(),
+                    right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击EditText的事件，忽略它。
+                return false;
+            } else {
+                return true;
+            }
+        }
+        // 如果焦点不是EditText则忽略，这个发生在视图刚绘制完，第一个焦点不在EditText上，和用户用轨迹球选择其他的焦点
+        return false;
+    }
+
+    private void hideKeyboard(IBinder token) {
+        if (token != null) {
+            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+}
